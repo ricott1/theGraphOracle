279f00c82edaffdb9cc95d30349e142e3e98e46f
ricott1 <ricott2@gmail.com>
2019-08-24 10:44:52 +0200

    updated testcase, docs

---
+++ contracts/TestCase.sol
@@ -0,0 +1,110 @@
+pragma solidity ^0.5.0;
+
+import "./theGraphOracle.sol";
+
+/**
+ * The TestCase contract to test theGraphOracle queries.
+ *
+* MIT License
+* 
+* Copyright (c) 2019 Alessandro Ricottone
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+ */
+contract TestCase {
+    
+    event BlockNumbersUpdated(uint[] result);
+    event KittyOwnersUpdated(address[] result);
+    
+    theGraphOracle public oracle;
+    uint[] public bns;
+    address[] public kittyOwners;
+    address public owner;
+
+    // Permits modifications only by the owner of the contract.
+    modifier only_owner() {
+        require(msg.sender == owner, "Only contract owner can call this function.");
+        _;
+    }
+
+    // Permits modifications only by the oracle contract.
+    modifier from_oracle() {
+        require(msg.sender == address(oracle), "Only oracle contract can call this function.");
+        _;
+    }
+    
+    /**
+     * @dev Constructs a new theGraphOracle contract.
+     */
+    constructor(address _oracleAddress) public {
+        oracle = theGraphOracle(_oracleAddress);
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Updates the theGraphOracle contract.
+     * @param _newOracle The new oracle address.
+     */
+    function updateOracle(address _newOracle) only_owner public {
+        oracle = theGraphOracle(_newOracle);
+    }
+
+    /**
+     * @dev Creates a query on theGraphOracle for ENS blockNumbers.
+     */
+    function queryENS() public {
+        string memory _company = "ensdomains";
+        string memory _product = "ens";
+        string memory _queryString = "{transfers(first:5){blockNumber}}";
+        bytes4 _callback = bytes4(keccak256("updateBlockNumber(uint256[])"));
+        oracle.createQuery(_company, _product, _queryString, address(this), _callback);
+    }
+    
+    /**
+     * @dev Callback function passed to the oracle.
+     * @param _result The result of the query.
+     */
+    function updateBlockNumber(uint[] calldata _result) from_oracle external {
+        require(_result.length == 5, "This function requires a length 5 array.");
+        emit BlockNumbersUpdated(_result);
+        bns = _result;
+    }
+    
+     /**
+     * @dev Creates a query on theGraphOracle for ENS blockNumbers.
+     */
+    function queryCryptoKitties() public {
+        string memory _company = "thomasproust";
+        string memory _product = "cryptokitties-explorer";
+        string memory _queryString = "{cryptoKitties(where:{birthTime_gte:1514761200,birthTime_lt:1519858800},first:10) {owner}}";
+        bytes4 _callback = bytes4(keccak256("updateBlockNumber(uint256[])"));
+        oracle.createQuery(_company, _product, _queryString, address(this), _callback);
+    }
+    
+    /**
+     * @dev Callback function passed to the oracle.
+     * @param _result The result of the query.
+     */
+    function updateKittyOwner(address[] calldata _result) from_oracle external {
+        require(_result.length == 10, "This function requires a length 10 array.");
+        emit KittyOwnersUpdated(_result);
+        kittyOwners = _result;
+    }
+    
+}
--- contracts/theGraphOracle.sol
+++ contracts/theGraphOracle.sol
@@ -1,19 +1,62 @@
 pragma solidity ^0.5.0;
 
+
+/**
+ * theGraphOracle contract to oraclize the Graph protocol.
+*
+* MIT License
+* 
+* Copyright (c) 2019 Alessandro Ricottone
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+ */
 contract theGraphOracle {
 
     event QueryCreated(string company, string product, string queryString, address _queryContract, bytes4 _callback);
 
     address public oracleAddress;
-    
+
+    // Permits modifications only by the oracle address.
+    modifier only_oracle() {
+        require(msg.sender == oracleAddress, "Only oracle address can call this function.");
+        _;
+    }
+    /**
+     * @dev Sets oracle whitelisted address.
+     */
     constructor (address _oracleAddress) public {
         oracleAddress = _oracleAddress;
     }
 
+    /**
+    * @dev Emits query with passed parameters. Event log is then intercepted from the off-chain oracle that performs the query and call the updateQuery function.
+    */
     function createQuery (string calldata _company, string calldata _product, string calldata _queryString, address _queryContract, bytes4 _callback) external {
         emit QueryCreated(_company, _product, _queryString, _queryContract, _callback);
     }
 
+    /**
+    * @dev Updates the contract that created the query by calling the specified callback function and passing the result as a parameter.
+    * @param _queryContract The address of the contract that created the query.
+    * @param _callback The Method ID passed in the original query.
+    * @param _result The result as a dynamic sized array (we use function overloading to catch many possible variable types)
+    */
     function updateQuery(address _queryContract, bytes4 _callback, uint[] memory _result) public returns (bool){
         require(msg.sender == oracleAddress);
         (bool status,) = _queryContract.call(abi.encodePacked(_callback, uint(32), uint(_result.length), _result));
@@ -21,12 +64,36 @@ contract theGraphOracle {
         return true;
     }
     
-    function updateQuery(address _queryContract, bytes4 _callback, address[] memory _result) public returns (bool){
+    function updateQuery(address _queryContract, bytes4 _callback, int[] memory _result) public returns (bool){
         require(msg.sender == oracleAddress);
         (bool status,) = _queryContract.call(abi.encodePacked(_callback, uint(32), uint(_result.length), _result));
         require(status);
         return true;
     }
+    
+    function updateQuery(address _queryContract, bytes4 _callback, bool[] memory _result) only_oracle public returns (bool){
+        (bool status,) = _queryContract.call(abi.encodePacked(_callback, uint(32), uint(_result.length), _result));
+        require(status);
+        return true;
+    }
+    
+    function updateQuery(address _queryContract, bytes4 _callback, address[] memory _result) only_oracle public returns (bool){
+        (bool status,) = _queryContract.call(abi.encodePacked(_callback, uint(32), uint(_result.length), _result));
+        require(status);
+        return true;
+    }
 
+    function updateQuery(address _queryContract, bytes4 _callback, string memory _result) only_oracle public returns (bool){
+        //bytes(_result).length not always returns expected result. Use with extreme caution (or just don't return string).
+        (bool status,) = _queryContract.call(abi.encodePacked(_callback, uint(32), uint(bytes(_result).length), _result));
+        require(status);
+        return true;
+    }
+    
+    function updateQuery(address _queryContract, bytes4 _callback, bytes memory _result) only_oracle public returns (bool){
+        (bool status,) = _queryContract.call(abi.encodePacked(_callback, uint(32), uint(_result.length), _result));
+        require(status);
+        return true;
+    }
     
 }
